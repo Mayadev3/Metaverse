e7b99feaab574b62fbe4fe184f084154028c9769
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-08-12 14:05:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*function changeText() {
+  document.getElementById("changing").innerHTML = "Yes";
+}
+function changeTextBack() {
+  document.getElementById("changing").innerHTML = "My First Paragraph";
+}*/
+
+/*function changeBackgroundColor() {
+  document.getElementById("maya1").style.backgroundColor = "#5A0427";
+}*/
